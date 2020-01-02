06de28ef15d49badf95583dcf77dd8eed6d99cd3
alex-aleyan <37502815+alex-aleyan@users.noreply.github.com>
2020-01-02 12:13:17 -0500

    centos6 and centos7 are handled now
    centos6 and centos7 are handled now

--- install_32bit_libs.sh
+++ install_32bit_libs.sh
@@ -1,35 +1,57 @@
 #!/bin/bash
 
-install_these_libs=(
-libc.i686
-glibc.i686
-libXext.i686
-libX11.i686
-libXau.i686
-libXdmcp.i686
-freetype.i686
-fontconfig.i686
-libpng.i686 
-libSM.i686
-expat.i686
-libXft.i686
-ncurses-libs.i686
-libstdc++*
-libstdc++*686
-unixODBC-libs*i686
-unixODBC*i686
-ncurses*i686
-ncurses-libs*i686
-libzmq3*i686
-libXext*i686
-alsalib*i686
-libXtst*i686
-libXft*i686
-libxml2*i686
-libedit*i686
-libX11*i686
-libXi*i686
-)
+#lsb_release -r | sed -n -e 's/^Release:\s//p'
+
+# Determine if we running centos6 or centos7
+centos_version="$( lsb_release -r | sed -n -e 's/^Release:\s//p' \
+                                  | awk -v RS=[0-9]+ '{print RT+0;exit}'  )"
+
+if [[ $centos_version -eq 6 ]]; then
+    echo "Ver: $centos_version"
+    libs_32bit=(
+        libc.i686
+        glibc.i686
+        libXext.i686
+        libX11.i686
+        libXau.i686
+        libXdmcp.i686
+        freetype.i686
+        fontconfig.i686
+        libpng.i686 
+        libSM.i686
+        expat.i686
+        libXft.i686
+        ncurses-libs.i686
+        libstdc++*
+        libstdc++*686
+    )
+
+elif [[ $centos_version -eq 7 ]]; then
+    echo "Ver: $centos_version"
+    
+    libs_32bit=(
+        unixODBC-libs*i686
+        unixODBC*i686
+        ncurses*i686
+        ncurses-libs*i686
+        libzmq3*i686
+        libXext*i686
+        alsa-lib*i686
+        libXtst*i686
+        libXft*i686
+        libxml2*i686
+        libedit*i686
+        libX11*i686
+        libXi*i686
+    )
+
+fi
+
+echo "${libs_32bit[@]}"
+
+exit
+
+
 
 install_32_libs=(
 glibc:i386
@@ -46,5 +68,4 @@ libXft.i386
 ncurses-libs.i386
 )
 
-yum install ${install_these_libs[@]}
-#yum install ${install_32_libs[@]}
+yum install ${libs_32bit[@]}
