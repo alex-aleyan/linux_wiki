47776397ecf8142e9284125121cd51e647d9964d
alex-aleyan <sasha.aleyan@gmail.com>
2020-01-10 13:31:52 -0500

    added safety checks; still need to put the groups into a single array

--- install_minimal_desktop.sh
+++ install_minimal_desktop.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Determine if we running centos6 or centos7
+centos_version="$( lsb_release -r | sed -n -e 's/^Release:\s//p' \
+                                  | awk -v RS=[0-9]+ '{print RT+0;exit}'  )"
+
+if [[ -z $centos_version ]]; then echo "ERROR: Install lsb_release!"; exit -1; fi
+
+if [[ $centos_version -eq 7 ]]; then
+echo "Ver: $centos_version"
 install_these_libs=(
 gnome-classic-session
 gnome-terminal
@@ -7,8 +15,11 @@ nautilus-open-terminal
 control-center
 liberation-mono-fonts
 )
+fi
 
-yum groupinstall "X Window System" "fonts"
+yum groupinstall "X Window System"
+if [ $? -ne 0 ]; then exit -1; fi
+yum groupinstall "fonts"
 if [ $? -ne 0 ]; then exit -1; fi
 
 yum install ${install_these_libs[@]}
