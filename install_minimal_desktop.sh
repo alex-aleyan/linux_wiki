1ba7714ae2ca053c0b6b8182e84802a0cc5f17eb
alex-aleyan <37502815+alex-aleyan@users.noreply.github.com>
2020-01-11 13:51:32 -0500

    modified yum to use a single *centos7* repo

--- install_minimal_desktop.sh
+++ install_minimal_desktop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+yum_centos_repo='yum -y --disablerepo="*" --enablerepo="*centos_7*"'
+
 # Determine if we running centos6 or centos7
 centos_version="$( lsb_release -r | sed -n -e 's/^Release:\s//p' \
                                   | awk -v RS=[0-9]+ '{print RT+0;exit}'  )"
@@ -17,12 +19,12 @@ liberation-mono-fonts
 )
 fi
 
-yum groupinstall "X Window System"
+yum_centos_repo groupinstall "X Window System"
 if [ $? -ne 0 ]; then exit -1; fi
-yum groupinstall "fonts"
+yum_centos_repo groupinstall "fonts"
 if [ $? -ne 0 ]; then exit -1; fi
 
-yum install ${install_these_libs[@]}
+yum_centos_repo install ${install_these_libs[@]}
 if [ $? -ne 0 ]; then exit -1; fi
 
 ln -fs /lib/systemd/system/graphical.target /etc/systemd/system/default.target
