a181dae40f76716a828fac7c0edfbe4194bfa102
alex-aleyan <37502815+alex-aleyan@users.noreply.github.com>
2020-01-11 00:26:13 -0500

    Update install_centos7_repos.sh

--- install_centos7_repos.sh
+++ install_centos7_repos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 yum-config-manager --add-repo http://mirror.centos.org/centos-7/7.7.1908/os/x86_64
-yum install http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install https://dl.fedoraproject.org/pub/epel/7/x86_64
 yum install http://build.openhpc.community/OpenHPC:/1.3/CentOS_7/x86_64/ohpc-release-1.3-1.el7.x86_64.rpm
