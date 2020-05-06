b7d214a6cce28883786d0cb8917f156936c6e9b1
alex-aleyan <37502815+alex-aleyan@users.noreply.github.com>
2020-05-06 12:53:00 -0400

    cleaned up

--- winrc.bat
+++ winrc.bat
@@ -1,35 +1,23 @@
 @echo off
 
-REM place it at "C:\Users\<USERNAME>\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\winrc.bat"
-
+rem Set path:
 set tcl_shell=C:\Tcl\bin
 call :doesFileExist %tcl_shell%
 
 set modelsim_exec=C:\modeltech64_10.5g\win64
 call :doesFileExist %modelsim_exec%
 
-set LM_LICENSE_FILE=<YOUR LICENSE HERE>
-
-REM PUT THIS INTO A FUNCTION!!!
-set QSYS_ROOTDIR=C:\intelFPGA_pro\18.0\qsys
-call :doesFileExist %QSYS_ROOTDIR%
-setx QSYS_ROOTDIR %QSYS_ROOTDIR%
-
-set QUARTUS_ROOTDIR=C:\intelFPGA_pro\18.0\quartus
-call :doesFileExist %QUARTUS_ROOTDIR%
-setx QUARTUS_ROOTDIR %QUARTUS_ROOTDIR%
-
-rem set SOPC_KIT_NIOS2=C:\intelFPGA_pro\18.0\nios2eds
-rem call :doesFileExist %SOPC_KIT_NIOS2%
-rem setx SOPC_KIT_NIOS2 %SOPC_KIT_NIOS2%
-
-call :setVar SOPC_KIT_NIOS2 C:\intelFPGA_pro\18.0\nios2eds
-
-rem set QSYS_ROOTDIR=C:\intelFPGA\16.1\quartus\sopc_builder\bin
-rem call :doesFileExist %QSYS_ROOTDIR%
-setx QSYS_ROOTDIR %QSYS_ROOTDIR%
-
 setx Path "%tcl_shell%;%modelsim_exec%"
+
+rem set LM_LICENSE_FILE=27001@ms2altera.syr.lmco.com
+call :setVarToValue LM_LICENSE_FILE 27001@ms2altera.syr.lmco.com
+
+rem Set enviroment variable/path pairs:
+call :setVarToPath DVT_HOME C:\Eclipse\Eclipse-DVT-19.1.40-win_64\dvt_eclipse
+call :setVarToPath QSYS_ROOTDIR C:\intelFPGA_pro\18.0\qsys
+call :setVarToPath QUARTUS_ROOTDIR C:\intelFPGA_pro\18.0\quartus
+call :setVarToPath SOPC_KIT_NIOS2 C:\intelFPGA_pro\18.0\nios2eds
+call :setVarToPath QSYS_ROOTDIR %QSYS_ROOTDIR%
 
 
 set /p MESSAGE=Hit ENTER to finish...
@@ -39,9 +27,15 @@ set /p MESSAGE=Hit ENTER to finish...
   ELSE ( echo Can NOT find %~1 & pause & cmd /c exit -1073741510)
   EXIT /B 0
 
-: setVar
+:setVarToPath
   set thisVar=%~1
   set thisPath=%~2
   call :doesFileExist %thisPath%
+  setx %thisVar% %thisPath%
+  echo %thisVar%%thisPath%
+  
+: setVarToValue
+  set thisVar=%~1
+  set thisPath=%~2
   setx %thisVar% %thisPath%
   echo %thisVar%%thisPath%
