512125322782670de3f1d7ac7d7c24a93fd84b77
admin <sasha.aleyan@gmail.com>
2020-06-05 14:05:46 -0400

    miscl

--- install_32bit_libs.sh
+++ install_32bit_libs.sh
@@ -7,35 +7,35 @@
 centos_version="$( lsb_release -r | sed -n -e 's/^Release:\s//p' \
                                   | awk -v RS=[0-9]+ '{print RT+0;exit}'  )"
 
-if [[ $centos_version -eq 6 ]]; then
-    echo "Ver: $centos_version"
-    libs_32bit=(
-        libc.i686
-        glibc.i686
-        libXext.i686
-        libX11.i686
-        libXau.i686
-        libXdmcp.i686
-        freetype.i686
-        fontconfig.i686
-        libpng.i686 
-        libSM.i686
-        expat.i686
-        libXft.i686
-        ncurses-libs.i686
-        libstdc++*
-        libstdc++*686
-    )
+libs_32bit=(
+    libc.i686
+    glibc.i686
+    libXext.i686
+    libX11.i686
+    libXau.i686
+    libXdmcp.i686
+    freetype.i686
+    fontconfig.i686
+    libpng.i686 
+    libSM.i686
+    expat.i686
+    libXft.i686
+    ncurses-libs.i686
+    libstdc++*
+    libstdc++*686
+)
 
-elif [[ $centos_version -eq 7 ]]; then
+# if CentOS7, append next utils to the list of utils to install:
+if [[ $centos_version -eq 7 ]]; then
+
     echo "Ver: $centos_version"
     
     libs_32bit=(
-        unixODBC-libs*i686
-        unixODBC*i686
+        "${libs_32bit[@]}"
+        unixODBC-devel*i686
         ncurses*i686
         ncurses-libs*i686
-        libzmq3*i686
+        perl-ZMQ-LibZMQ3
         libXext*i686
         alsa-lib*i686
         libXtst*i686
@@ -45,15 +45,15 @@ elif [[ $centos_version -eq 7 ]]; then
         libX11*i686
         libXi*i686
     )
-
 fi
 
-echo "${libs_32bit[@]}"
+#for i in ${libs_32bit[@]}; do echo $i ; done
+
+yum install ${libs_32bit[@]}
 
 exit
 
-
-
+echo "${libs_32bit[@]}"
 install_32_libs=(
 glibc:i386
 libXext.i386
@@ -68,5 +68,4 @@ expat.i386
 libXft.i386
 ncurses-libs.i386
 )
-
 yum install ${libs_32bit[@]}
